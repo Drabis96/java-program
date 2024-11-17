4cbf7408ae014d99db1b5570e2d8fa214e9f5d78
Lukas Drabek <l.drabek@post.cz>
2024-11-17 18:58:32 +0100

    First commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Ahoj svete!");
+
+//        Promenne
+
+        int cislo2  = 4;
+        double desetine_cislo = 3.14;
+        String text = "Ahoj";
+        for (int i = 0; i < text.length(); i++) {
+            System.out.println("Znak na pozici " + i + ": " + text.charAt(i));
+        }
+
+    }
+}git
